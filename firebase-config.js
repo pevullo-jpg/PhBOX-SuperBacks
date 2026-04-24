9af432f367b51985b44e7276972ff3fdd79bfe00
pevullo-jpg <pevullo@gmail.com>
2026-04-24 10:18:30 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBSJoTKtjoLTT-Q8Q5iAWmtERyavIINjLs",
-  authDomain: "phbox-369e8.firebaseapp.com",
-  projectId: "phbox-369e8",
-  storageBucket: "phbox-369e8.firebasestorage.app",
-  messagingSenderId: "829413724215",
-  appId: "1:829413724215:web:8401e651b56b98f30fefb2",
-  measurementId: "G-VF8E5DQRCZ",
-};
